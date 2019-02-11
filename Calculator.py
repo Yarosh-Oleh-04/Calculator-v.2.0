484bbb65d1d04ef7327797cf383827eca7b8bcb0
Yarosh-Oleh-04 <yarosholeh04@gmail.com>
2019-02-11 21:09:05 +0200

    edit button styles

--- Calculator.py
+++ Calculator.py
@@ -32,41 +32,60 @@ def write(val):
             res.insert(0, result)
 
 
-
 top = tkinter.Tk()
-top.title('Window')
-top.geometry('400x145')
+top.title('Calculator')
+top.geometry('400x358')
 top.grid(widthInc=4)
+top.resizable(0, 0)
 
 text_field = tkinter.StringVar()
 
-form = tkinter.Entry(top, width=50, textvariable=text_field)
-form.grid(columnspan=4, row=0)
-res = tkinter.Entry(top, width=50)
-res.grid(columnspan=4, row=1)
+form = tkinter.Entry(top, width=30, textvariable=text_field, font=('', '18'))
+form.grid(columnspan=5, row=0)
+res = tkinter.Entry(top, width=30, font=('', '18'))
+res.grid(columnspan=5, row=1)
 buttons = []
 
 for i in range(9):
-    tkinter.Button(top, bg='gray', width=10, text=str(i+1), command=lambda x=i+1: write(x)).grid(row=i//3+2, column=i%3)
+    buttons += [tkinter.Button(top, text=str(i+1), command=lambda x=i+1: write(x))]
+    buttons[i].grid(row=i//3+2, column=i%3)
 
-equals = tkinter.Button(top, bg='gray', width=10, text='=', command=btn_click)
-equals.grid(row=0, column=4, rowspan=2)
-equals.config(height=2, width=10, text='=')
-tkinter.Button(top, bg='gray', width=10, text='+', command=lambda: write('+')).grid(row=2, column=3)
-tkinter.Button(top, bg='gray', width=10, text='-', command=lambda: write('-')).grid(row=3, column=3)
-tkinter.Button(top, bg='gray', width=10, text='*', command=lambda: write('*')).grid(row=4, column=3)
-tkinter.Button(top, bg='gray', width=10, text='/', command=lambda: write('/')).grid(row=5, column=3)
+buttons += [tkinter.Button(top, bg='gray', width=10, text='=', command=btn_click)]
+buttons[9].grid(row=5, column=2)
+buttons += [tkinter.Button(top, text='+', command=lambda: write('+'))]
+buttons += [tkinter.Button(top, text='-', command=lambda: write('-'))]
+buttons += [tkinter.Button(top, text='*', command=lambda: write('*'))]
+buttons += [tkinter.Button(top, text='/', command=lambda: write('/'))]
 
+for i in range(1, 5):
+    buttons[i+9].grid(row=i+1, column=3)
 
-tkinter.Button(top, bg='gray', width=10, text='C', command=lambda: write('c')).grid(row=3, column=4)
-tkinter.Button(top, bg='gray', width=10, text='Del', command=lambda: write('del')).grid(row=2, column=4)
-tkinter.Button(top, bg='gray', width=10, text='0', command=lambda: write(0)).grid(row=5, column=1)
-tkinter.Button(top, bg='gray', width=10, text='.', command=lambda: write('.')).grid(row=5, column=0)
+buttons += [tkinter.Button(top, text='C', command=lambda: write('c'))]
+buttons[14].grid(row=3, column=4)
+buttons += [tkinter.Button(top, text='Del', command=lambda: write('del'))]
+buttons[15].grid(row=2, column=4)
+buttons += [tkinter.Button(top, text='0', command=lambda: write(0))]
+buttons[16].grid(row=5, column=1)
+buttons += [tkinter.Button(top, text='.', command=lambda: write('.'))]
+buttons[17].grid(row=5, column=0)
 
-tkinter.Button(top, bg='gray', width=10, text='(', command=lambda: write('(')).grid(row=4, column=4)
-tkinter.Button(top, bg='gray', width=10, text=')', command=lambda: write(')')).grid(row=5, column=4)
-tkinter.Button(top, bg='gray', width=10, text='', command=lambda: write('')).grid(row=5, column=2)
+buttons += [tkinter.Button(top, text='(', command=lambda: write('('))]
+buttons[18].grid(row=4, column=4)
+buttons += [tkinter.Button(top, text=')', command=lambda: write(')'))]
+buttons[19].grid(row=5, column=4)
 
+# 0-8 and 16: numbers;
+# 9: equals;
+# 10-13: actions
+# 14-15 and 18-19: Del, C, ();
+# 15: dot
+# 20: is empty
+
+for button in buttons:
+    button['bg'] = '#00CCFF'
+    button['width'] = 5
+    button.config(height=2)
+    button['font'] = ('', '18')
 
 top.mainloop()
 
